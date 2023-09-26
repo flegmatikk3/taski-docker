e56ac61444f49c5423047dc09e572b7851b76a4e
Влада Мухатдинова <ladamuxa33@gmail.com>
2023-09-26 17:10:28 +0300

    flake8 solving

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -3,7 +3,8 @@ from http import HTTPStatus
 
 from api import models
 from django.test import Client, TestCase
- 
+
+
 class TaskiAPITestCase(TestCase):
     def setUp(self):
         self.guest_client = Client()
